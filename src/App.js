f930121ddaac6d18c7d224d20eedb80fadea800a
pradnya237 <pradnya237@solacetechnologies.co.in>
2021-09-07 15:34:58 +0530

    useState imported

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import React , {useState} from 'react';
 import './App.css';
-import Demo from './Demo'
 
-function App() {
+const App=() => {
+  const [inputList,setInputList]=useState("buy Tomato");
+  const itemEvent= () =>{};
   return (
-    <div className="App">
-        <p>
-           <code>Hello-World</code> 
-        </p>
-        <Demo></Demo>
+    <>
+    <div className="main-div">
+      <div className="center-div">
+        <br/>
+        <h1>To Do List</h1>
+        <br/>
+        <input type="text" placeholder="Add a item" onChange={itemEvent}/>
+        <button>X</button>
+        <ol>
+          <li>{inputList}</li>
+        </ol>
+        
+
+      </div>
         
       
     </div>
+    </>
   );
 }
 
